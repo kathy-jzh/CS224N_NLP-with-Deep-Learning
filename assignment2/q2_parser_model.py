6546f104ea4a15ac16af5866cdc60e3e3fe16314
Zehui <kathy_jzh@yahoo.com>
2019-01-20 20:30:13 -0500

    finished assignment 2

--- assignment2/q2_parser_model.py
+++ assignment2/q2_parser_model.py
@@ -1,4 +1,4 @@
-import cPickle
+import pickle
 import os
 import time
 import tensorflow as tf
@@ -53,10 +53,12 @@ class ParserModel(Model):
 
         (Don't change the variable names)
         """
-        self.input_placeholder = tf.placeholder(tf.float32, 
-                                                shape=(self.config.batch_size, self.config.n_features))
-        self.labels_placeholder = tf.placeholder(tf.int32, 
-                                                 shape=(self.config.batch_size, self.config.n_classes))
+        self.input_placeholder = tf.placeholder(tf.int32, 
+                                                shape=(None, self.config.n_features))
+        self.labels_placeholder = tf.placeholder(tf.float32, 
+                                                 shape=(None, self.config.n_classes))
+        self.dropout_placeholder = tf.placeholder(tf.float32)
+        
         
     def create_feed_dict(self, inputs_batch, labels_batch=None, dropout=0):
         """Creates the feed_dict for the dependency parser.
@@ -80,8 +82,18 @@ class ParserModel(Model):
         Returns:
             feed_dict: The feed dictionary mapping from placeholders to values.
         """
-        ### YOUR CODE HERE
-        ### END YOUR CODE
+        
+        if labels_batch is not None:
+            feed_dict = {
+                    self.input_placeholder: inputs_batch,
+                    self.labels_placeholder: labels_batch,
+                    self.dropout_placeholder: dropout,
+                    }
+        else:
+            feed_dict = {
+                    self.input_placeholder: inputs_batch,
+                    self.dropout_placeholder: dropout,
+                    }
         return feed_dict
 
     def add_embedding(self):
@@ -101,8 +113,11 @@ class ParserModel(Model):
         Returns:
             embeddings: tf.Tensor of shape (None, n_features*embed_size)
         """
-        ### YOUR CODE HERE
-        ### END YOUR CODE
+        embeddings_dic = tf.Variable(initial_value=self.pretrained_embeddings)
+        embeddings = tf.nn.embedding_lookup(embeddings_dic, self.input_placeholder)
+        embeddings = tf.reshape(embeddings, [-1, self.config.n_features*self.config.embed_size])
+        
+#        self.pretrained_embeddings = embeddings
         return embeddings
 
     def add_prediction_op(self):
@@ -125,10 +140,17 @@ class ParserModel(Model):
         Returns:
             pred: tf.Tensor of shape (batch_size, n_classes)
         """
-
         x = self.add_embedding()
-        ### YOUR CODE HERE
-        ### END YOUR CODE
+        xavier_initializer = xavier_weight_init()
+        W = xavier_initializer((self.config.n_features*self.config.embed_size, self.config.hidden_size))
+        b1 = tf.Variable(tf.zeros(self.config.hidden_size))
+        h = tf.nn.relu(tf.matmul(x ,W) + b1)
+        
+        U = xavier_initializer((self.config.hidden_size, self.config.n_classes))
+        b2 = tf.Variable(tf.zeros(self.config.n_classes))
+        h_drop = tf.nn.dropout(h, 1- self.dropout_placeholder)
+        pred = tf.matmul(h_drop,U) + b2
+        
         return pred
 
     def add_loss_op(self, pred):
@@ -144,8 +166,10 @@ class ParserModel(Model):
         Returns:
             loss: A 0-d tensor (scalar)
         """
-        ### YOUR CODE HERE
-        ### END YOUR CODE
+        
+        loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels= self.labels_placeholder,
+                                                                      logits = pred))
+        
         return loss
 
     def add_training_op(self, loss):
@@ -168,8 +192,8 @@ class ParserModel(Model):
         Returns:
             train_op: The Op for training.
         """
-        ### YOUR CODE HERE
-        ### END YOUR CODE
+        train_op = tf.train.AdamOptimizer(self.config.lr).minimize(loss)
+        
         return train_op
 
     def train_on_batch(self, sess, inputs_batch, labels_batch):
@@ -183,9 +207,11 @@ class ParserModel(Model):
         prog = tf.keras.utils.Progbar(target=n_minibatches)
         for i, (train_x, train_y) in enumerate(minibatches(train_examples, self.config.batch_size)):
             loss = self.train_on_batch(sess, train_x, train_y)
-            prog.update(i + 1, [("train loss", loss)], force=i + 1 == n_minibatches)
-
-        print ("Evaluating on dev set", dev_UAS, _ = parser.parse(dev_set))
+            prog.update(i + 1, [("train loss", loss)])
+#        prog.update(i, force = (i + 1 == n_minibatches))
+        
+        print ("Evaluating on dev set")
+        dev_UAS, _ = parser.parse(dev_set)
         print ("- dev UAS: {:.2f}".format(dev_UAS * 100.0))
         return dev_UAS
 
@@ -241,14 +267,15 @@ def main(debug=True):
             print (80 * "=")
             print ("Restoring the best model weights found on the dev set")
             saver.restore(session, './data/weights/parser.weights')
-            print ("Final evaluation on test set", UAS, dependencies = parser.parse(test_set))
+            print ("Final evaluation on test set")
+            UAS, dependencies = parser.parse(test_set)
             print ("- test UAS: {:.2f}".format(UAS * 100.0))
             print ("Writing predictions")
             with open('q2_test.predicted.pkl', 'w') as f:
-                cPickle.dump(dependencies, f, -1)
+                pickle.dump(dependencies, f, -1)
             print ("Done!")
 
 
 if __name__ == '__main__':
-    main()
+    main(False)
 
